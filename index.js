dd9f4bfc4c5d2710e45ee5ee93011ac9940aff76
Jose Leal <75815969+joseferleal@users.noreply.github.com>
2023-03-23 17:00:46 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+//Las clases son moldes
+
+{nombreCliente; dniCliente; numeroCuenta; saldoCuenta}
+
+
+const nombreCliente="Leonardo";
+const dniCliente=138464;
+const numeroCuenta=64556464;
+const saldoCuenta=1000;
