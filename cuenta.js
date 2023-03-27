43b7e8c171096d277ed27e389b821a59b160e10a
Joseferleal <joseferleal@gmail.com>
2023-03-27 17:26:44 -0500

    Generalización y especificación de clases

--- cuenta.js
+++ cuenta.js
@@ -1,1 +1,45 @@
-export class cuenta{};
+export class cuenta{
+
+    #cliente;
+    #saldo; 
+    constructor(tipo, numero, agencia, cliente, saldo){
+       this.tipo=tipo;
+       this.numero=numero;
+       this.agencia=agencia;
+       this.#cliente=cliente;
+       this.#saldo=saldo;
+       
+    };
+
+
+    depositoEnCuenta(valor){
+        /* this.saldo=this.saldo+valor; */
+        if(valor>0){this.#saldo+=valor; return this.#saldo;}else{console.log("No es posible dicho depósito"); return this.#saldo;};
+        
+    };
+
+    retirarDeCuenta(valor){
+        if(this.tipo=="Corriente"){valor=valor*1.05;}else if(this.tipo="Ahorros"){valor=valor*1.02};
+        if(valor<=this.#saldo){this.#saldo-=valor; return this.#saldo;
+        }else{console.log("No es posible dicho retiro"); return this.#saldo;};
+        
+    };
+
+
+    verSaldo(){
+        return this.#saldo;
+    };
+
+
+    transferirParaCuenta(valor, cuentaDestino){
+        if(valor<=this.#saldo){
+            this.retirarDeCuenta(valor);
+            cuentaDestino.depositoEnCuenta(valor);
+        }
+        else{console.log("No es posible dicha transferencia. Fondos insuficientes")};
+        
+
+    }
+
+
+};
--- ejemplos.js
+++ ejemplos.js
@@ -17,48 +17,62 @@ cliente2.dniCliente=4266664;
 cliente2.rutCliente=434437;
  */
 
-const cuentaLeonardo= new cuentaCorriente(cliente, 1, 1001);
+/*const cuentaLeonardo= new cuentaCorriente(cliente, 1, 1001);
 /* cuentaLeonardo.numero=1;
 cuentaLeonardo.agencia=1001;
 cuentaLeonardo.setCliente=cliente;
  */
-const cuentaMaria= new cuentaCorriente(cliente2, 2, 1002);
+/*const cuentaMaria= new cuentaCorriente(cliente2, 2, 1002);
 /* cuentaMaria.numero=2;
 cuentaMaria.agencia=1002;
 cuentaMaria.setCliente=cliente2; */
 
 
-console.log(cuentaLeonardo);
+/* console.log(cuentaLeonardo);
 let saldo=cuentaLeonardo.verSaldo();
 console.log("el saldo actual es " + saldo);
+ */
+
 /* cuentaLeonardo.saldo=0; ya no lo necesito, porque tengo un constructor */
-cuentaLeonardo.depositoEnCuenta(150);
-saldo=cuentaLeonardo.verSaldo(); ///Se puede así
-console.log("el saldo actual es " + saldo);
+
+//cuentaLeonardo.depositoEnCuenta(150);
+//saldo=cuentaLeonardo.verSaldo(); ///Se puede así
+/* console.log("el saldo actual es " + saldo);
 saldo=cuentaLeonardo.retirarDeCuenta(70);
 console.log("el saldo actual es " + saldo);
 saldo=cuentaLeonardo.retirarDeCuenta(70);
 console.log("el saldo actual es " + saldo);
 saldo=cuentaLeonardo.depositoEnCuenta(90);
 console.log("el saldo actual es " + saldo);
-
+ */
 ///Instancia del cliente María
-cuentaLeonardo.transferirParaCuenta(100, cuentaMaria)
+/* cuentaLeonardo.transferirParaCuenta(100, cuentaMaria)
 const saldoMaria=cuentaMaria.verSaldo();
 console.log("el saldo actual de María es  " + saldoMaria);
 const saldoLeonardo=cuentaLeonardo.verSaldo();
 console.log("el saldo actual de Leonardo es " + saldoLeonardo);
 console.log(cuentaMaria);
-console.log(cuentaMaria.cliente);
+console.log(cuentaMaria.cliente); */
 
 //Conteo del total de cuentas en todo el banco
-console.log(cuentaCorriente.cantidadCuentas);
+/* console.log(cuentaCorriente.cantidadCuentas);
 new cuentaCorriente(cliente, 22, 1001);
-console.log(cuentaCorriente.cantidadCuentas);
+console.log(cuentaCorriente.cantidadCuentas); */
 
 
 
 ////////////////////////////Cuenta de ahorros
 
-const cuentaAhorroLeonardo= new cuentaAhorro(cliente, 1001);
+const cuentaAhorroLeonardo= new cuenta("Ahorros", 1, 1001, cliente, 5000);
 console.log(cuentaAhorroLeonardo);
+
+const cuentaMaria= new cuenta(2, 1002, cliente2,6000);
+console.log(cuentaMaria);
+
+
+const cuentaLeonardo= new cuenta("Corriente", 2, 1001, cliente, 4000);
+cuentaAhorroLeonardo.depositoEnCuenta(150);
+cuentaAhorroLeonardo.retirarDeCuenta(50);
+console.log(cuentaAhorroLeonardo.verSaldo());
+cuentaLeonardo.retirarDeCuenta(1000);
+console.log(cuentaLeonardo.verSaldo());
